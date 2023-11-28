c12033d020be316fdb543057dded958a8320705f
pavel <pavelkorotkihhh@gmail.com>
2023-11-28 16:38:33 +0300

    second commit

--- apps/account-service/src/app/app.module.ts
+++ apps/account-service/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import {Module} from "@nestjs/common";
 import {MongooseModule} from "@nestjs/mongoose";
-import {mongooseConnect, getConfigModule} from "@libs/config/src";
+import {mongooseConnect, getConfigModule} from "configs";
 
 import {AppController} from "./app.controller";
 import {AppService} from "./app.service";
